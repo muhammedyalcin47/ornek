ea28e1b4011a7b55686d35819f4b4da6b221af63
muhammed yalçın <muhammedyalcin4721@gmail.com>
2021-11-27 23:16:45 +0300

    String metodları eklendi

---
+++ src/com/bilgeadam/ornek/AdSoyadAyırma.java
@@ -0,0 +1,23 @@
+package com.bilgeadam.ornek;
+
+public class AdSoyadAyırma {
+    public static void main(String[] args) {
+
+
+        String isim = "Aziz Sancar";
+
+        int boslukIndex = isim.indexOf(" ");
+
+        String ad = isim.substring(0, boslukIndex);
+
+        System.out.println("Ad:"+ad+" Uzunluk:"+ad.length());
+
+        String soyad = isim.substring(boslukIndex+1);
+
+        System.out.println("Soyad:"+soyad+" Uzunluk:"+soyad.length());
+
+
+
+
+    }
+}
